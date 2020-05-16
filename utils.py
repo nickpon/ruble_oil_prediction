03537915de68c40b0247c877b5aa9680a359bf55
Nikolai Ponomarenko <nickpon@yandex-team.ru>
2020-05-16 19:28:18 +0300

    First results on CatBoost

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error
 import torch
 
 
@@ -8,3 +10,11 @@ def get_device() -> torch.device:
     else:
         print('Using {} GPUs'.format(torch.cuda.get_device_name(0)))
     return device
+
+
+def mape(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+def rmse(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    return np.sqrt(mean_squared_error(y_true=y_true, y_pred=y_pred))
